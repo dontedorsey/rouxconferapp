20b814698c13dd8cb43a0a7a658ebe29d4278956
Darrold Dorsey <dontedorsey@gmail.com>
2015-11-05 16:21:05 -0500

    default header include

---
+++ components/php/header.php
@@ -0,0 +1,10 @@
+<div class="content row">
+<div class="col-lg-12">
+  <header class="clearfix">
+  <section id="branding">
+    <a href="../../index.php"><img src="../../img/misc/ralogo_monogram.png" alt="Logo for Roux Con"</a>
+    </section><!-- branding -->
+  </header><!-- header -->
+  </div><!-- column -->
+
+</div><!-- content -->
