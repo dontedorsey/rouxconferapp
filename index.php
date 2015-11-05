0f4f5bd857d731511f02ae6e502ddfc91997b84c
Darrold Dorsey <dontedorsey@gmail.com>
2015-11-05 15:50:34 -0500

    Making sure it works

--- index.php
+++ index.php
@@ -1,27 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Roux Conference App</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/style.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-  </head>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Roux Academy Conference 2013 -- Home</title>
 
-  <body>
+  <!-- Bootstrap core CSS -->
+  <link href="css/style.css" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
 
-<h1> This is something</h1>
+<body id="home">
+
+  <section class="container">
+    <div class="content row">
+      <section class="main col col-lg-8">
+        <h2>Main Content</h2>
+        <p>Non sed comprehenderit.</p>
+      </section>
+      <!-- main -->
+      <section class="sidebar col-lg-4">
+        <h2>SideBar Content</h2>
+        <p>Non sed comprehenderit.</p>
+      </section>
+      <!-- sidebar -->
+    </div>
+    <!-- content -->
+  </section>
+  <!--container-->
 
 
 
-    <!-- Bootstrap core JavaScript
+  <!-- Bootstrap core JavaScript
     ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery-1.11.3.js"></script>
-    <script src="js/bootstrap.js"></script>
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="js/jquery-1.11.3.js"></script>
+  <script src="js/bootstrap.js"></script>
 
-  </body>
+</body>
+
 </html>
-
